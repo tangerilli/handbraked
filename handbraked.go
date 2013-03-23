ef285ed276ae90a882a97030af98051faeeff0bf
Tony Angerilli <tony@angerilli.ca>
2013-03-23 15:16:53 -0700

    Process files added to the watch directory as they're added

--- handbraked.go
+++ handbraked.go
@@ -5,7 +5,29 @@ import ("fmt"
         "os"
         "path/filepath"
         "os/exec"
+        "regexp"
+        "strconv"
+        "github.com/howeyc/fsnotify"
+        "log"
+        "time"
+        "sort"
+        "strings"
 )
+
+const outputPath = "/tmp"
+var movieExtensions []string = []string{"m4v", "avi", "mpg", "mpeg", "mkv", "mov"}
+
+func FindFileTypes(path string, extensions []string) []string {
+    var results []string
+    for _, pattern := range extensions {
+        r, err := filepath.Glob(filepath.Join(path, "*." + pattern))
+        if err != nil {
+            continue
+        }
+        results = append(results, r...)
+    }
+    return results
+}
 
 func usage() {
     fmt.Println("usage: handbraked [path to watch]")
@@ -13,21 +35,12 @@ func usage() {
     os.Exit(1)
 }
 
-func findMovies(path string) []string {
-    var results []string
-    formats := []string{"m4v", "avi", "mpg", "mpeg", "mkv"}
-    for _, pattern := range formats {
-        r, _ := filepath.Glob(filepath.Join(path, "*." + pattern))
-        results = append(results, r...)
-    }
-    return results
-}
-
-func processFile(path string, c chan string) {
+func processFile(path string, c chan float64) {
     defer close(c)
 
-    fmt.Println("Doing stuff with " + path)
-    cmd := exec.Command("/usr/bin/file", path)
+    fmt.Println("Processing " + path)
+    outputFile := filepath.Join(outputPath, filepath.Base(path))
+    cmd := exec.Command("HandBrakeCLI", "-i", path, "--preset=iPad", "-o", outputFile)
     stdout, err := cmd.StdoutPipe()
     if err != nil {
         fmt.Println("Error opening stdout")
@@ -36,9 +49,17 @@ func processFile(path string, c chan string) {
 
     go func() {
         b := make([]byte, 1024)
+        pattern, err := regexp.Compile("\\d+\\.\\d+")
+        if err != nil {
+            fmt.Println("Error compiling pattern")
+        }
         for l, err := stdout.Read(b); l > 0 || err == nil; l, err = stdout.Read(b) {
             if l > 0 {
-                c <- string(b)
+                percentageStr := pattern.FindString(string(b))
+                percentage, err := strconv.ParseFloat(percentageStr, 32)
+                if err == nil {
+                    c <- percentage
+                }
             }
         }
     }()
@@ -54,6 +75,15 @@ func processFile(path string, c chan string) {
     }
 }
 
+func HandleFile(file string) {
+    c := make(chan float64)
+    go processFile(file, c)
+    for output := range c {
+        fmt.Println(output)
+    }
+    fmt.Println("Finished " + file)
+}
+
 func main() {
     flag.Usage = usage
     flag.Parse()
@@ -63,13 +93,48 @@ func main() {
         os.Exit(1)
     }
 
-    files := findMovies(args[0])
-    for _, file := range files {
-        c := make(chan string)
-        go processFile(file, c)
-        for output := range c {
-            fmt.Println(output)
-        }
-        fmt.Println("Finished " + file)
+    // Sorted so it can be searched later
+    sort.Strings(movieExtensions)
+
+    watcher, err := fsnotify.NewWatcher()
+    if err != nil {
+        log.Fatal(err)
     }
+
+    go func() {
+        for {
+            select {
+            case ev := <-watcher.Event:
+                log.Println("event:", ev)
+                if !ev.IsCreate() {
+                    continue
+                }
+                log.Println("event:", ev)
+
+                ext := strings.ToLower(filepath.Ext(ev.Name)[1:])
+                i := sort.SearchStrings(movieExtensions, ext)
+                if i < len(movieExtensions) && movieExtensions[i] == ext {
+                    HandleFile(ev.Name)
+                }
+            case err := <-watcher.Error:
+                log.Println("error:", err)
+            }
+        }
+    }()
+
+    // Iterate through and process any existing movie files
+    for _, file := range FindFileTypes(args[0], movieExtensions) {
+        HandleFile(file)
+    }
+
+    err = watcher.Watch(args[0])
+    if err != nil {
+        log.Fatal(err)
+    }
+    log.Println("Watching ", args[0])
+    for {
+        time.Sleep(10 * time.Second)
+    }
+
+    watcher.Close()
 }
