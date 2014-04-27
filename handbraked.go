e0ac974dd17062d38437cf1ab77ffd979d0aff16
Tony Angerilli <tony@angerilli.ca>
2014-04-26 17:07:20 -0700

    - Fixed a bug which prevented the first file in a directory from showing up  - Added an option to specify the location of the 'static' directory

--- handbraked.go
+++ handbraked.go
@@ -1,163 +1,173 @@
 package main
 
-import ("fmt"
-        "flag"
-        "os"
-        "path/filepath"
-        "os/exec"
-        "regexp"
-        "strconv"
-        "github.com/howeyc/fsnotify"
-        "log"
-        "sort"
-        "strings"
-        "github.com/tangerilli/handbraked/ui"
-        "github.com/tangerilli/handbraked/common"
-        "encoding/json"
-        "time"
+import (
+	"encoding/json"
+	"flag"
+	"fmt"
+	"github.com/howeyc/fsnotify"
+	"github.com/tangerilli/handbraked/common"
+	"github.com/tangerilli/handbraked/ui"
+	"log"
+	"os"
+	"os/exec"
+	"path/filepath"
+	"regexp"
+	"sort"
+	"strconv"
+	"strings"
+	"time"
 )
 
 var movieExtensions []string = []string{"m4v", "avi", "mpg", "mpeg", "mkv", "mov"}
 
 func usage() {
-    fmt.Println("usage: handbraked [path to watch] [output path] [movie file path]")
-    flag.PrintDefaults()
-    os.Exit(1)
+	fmt.Println("usage: handbraked [path to watch] [output path] [movie file path]")
+	flag.PrintDefaults()
+	os.Exit(1)
 }
 
 func processFile(path string, outputPath string, c chan float64) {
-    defer close(c)
+	defer close(c)
 
-    fmt.Println("Processing " + path)
-    newFileName := filepath.Base(path)
-    newFileName = newFileName[:len(newFileName)-3] + "m4v"
-    outputFile := filepath.Join(outputPath, newFileName)
-    tmpOutputFile := filepath.Join("/tmp", newFileName)
-    cmd := exec.Command("HandBrakeCLI", "-i", path, "--preset=iPad", "-o", tmpOutputFile)
-    stdout, err := cmd.StdoutPipe()
-    if err != nil {
-        fmt.Println("Error opening stdout")
-        return
-    }
+	fmt.Println("Processing " + path)
+	newFileName := filepath.Base(path)
+	newFileName = newFileName[:len(newFileName)-3] + "m4v"
+	outputFile := filepath.Join(outputPath, newFileName)
+	tmpOutputFile := filepath.Join("/tmp", newFileName)
+	cmd := exec.Command("HandBrakeCLI", "-i", path, "--preset=iPad", "-o", tmpOutputFile)
+	stdout, err := cmd.StdoutPipe()
+	if err != nil {
+		fmt.Println("Error opening stdout")
+		return
+	}
 
-    go func() {
-        b := make([]byte, 1024)
-        pattern, err := regexp.Compile("\\d+\\.\\d+")
-        if err != nil {
-            fmt.Println("Error compiling pattern")
-        }
-        for l, err := stdout.Read(b); l > 0 || err == nil; l, err = stdout.Read(b) {
-            if l > 0 {
-                percentageStr := pattern.FindString(string(b))
-                percentage, err := strconv.ParseFloat(percentageStr, 32)
-                if err == nil {
-                    c <- percentage
-                }
-            }
-        }
-    }()
+	go func() {
+		b := make([]byte, 1024)
+		pattern, err := regexp.Compile("\\d+\\.\\d+")
+		if err != nil {
+			fmt.Println("Error compiling pattern")
+		}
+		for l, err := stdout.Read(b); l > 0 || err == nil; l, err = stdout.Read(b) {
+			if l > 0 {
+				percentageStr := pattern.FindString(string(b))
+				percentage, err := strconv.ParseFloat(percentageStr, 32)
+				if err == nil {
+					c <- percentage
+				}
+			}
+		}
+	}()
 
-    if err := cmd.Start(); err != nil {
-        fmt.Println("Error starting")
-        return
-    }
+	if err := cmd.Start(); err != nil {
+		fmt.Println("Error starting")
+		return
+	}
 
-    if err := cmd.Wait(); err != nil {
-        fmt.Println("Error waiting")
-        return
-    }
-    os.Rename(tmpOutputFile, outputFile)
+	if err := cmd.Wait(); err != nil {
+		fmt.Println("Error waiting")
+		return
+	}
+	fmt.Printf("Renaming %s to %s\n", tmpOutputFile, outputFile)
+	err = os.Rename(tmpOutputFile, outputFile)
+	if err != nil {
+		fmt.Println("Error renaming file: ", err.Error())
+	}
 }
 
 type StatusUpdate struct {
-    Name string
-    Progress float64
+	Name     string
+	Progress float64
 }
 
 func HandleFile(file string, outputPath string, deleteOnCompletion bool) {
-    c := make(chan float64)
-    go processFile(file, outputPath, c)
-    status := StatusUpdate{filepath.Base(file), 0}
-    last := time.Now().Unix()
-    for output := range c {
-        fmt.Println(output)
-        now := time.Now().Unix()
-        if now - last < 2 {
-            continue
-        }
-        last = time.Now().Unix()
-        
-        status.Progress = output
-        j, err := json.Marshal(status)
-        if err != nil {
-            continue
-        }
-        ui.MessageHub.Broadcast <- string(j)
-    }
-    fmt.Println("Finished " + file)
-    if deleteOnCompletion {
-        os.Remove(file)
-    }
+	c := make(chan float64)
+	go processFile(file, outputPath, c)
+	status := StatusUpdate{filepath.Base(file), 0}
+	last := time.Now().Unix()
+	for output := range c {
+		fmt.Println(output)
+		now := time.Now().Unix()
+		if now-last < 2 {
+			continue
+		}
+		last = time.Now().Unix()
+
+		status.Progress = output
+		j, err := json.Marshal(status)
+		if err != nil {
+			continue
+		}
+		ui.MessageHub.Broadcast <- string(j)
+	}
+	fmt.Println("Finished " + file)
+	if deleteOnCompletion {
+		os.Remove(file)
+	}
 }
 
 func main() {
-    flag.Usage = usage
-    var deleteOnCompletion = flag.Bool("delete", true, "Delete files after processing")
-    flag.Parse()
-    args := flag.Args()
-    if len(args) < 3 {
-        usage()
-    }
-    inputPath := args[0]
-    outputPath := args[1]
-    moviePath := args[2]
+	flag.Usage = usage
+	var deleteOnCompletion = flag.Bool("delete", true, "Delete files after processing")
+	var staticPath = flag.String("static", "/static/", "Path to directory containing static website files")
+	flag.Parse()
+	args := flag.Args()
+	if len(args) < 3 {
+		usage()
+	}
+	inputPath := args[0]
+	outputPath := args[1]
+	moviePath := args[2]
 
-    // Sorted so it can be searched later
-    sort.Strings(movieExtensions)
+	if moviePath[len(moviePath)-1] == "/"[0] {
+		moviePath = moviePath[:len(moviePath)-1]
+	}
 
-    watcher, err := fsnotify.NewWatcher()
-    if err != nil {
-        log.Fatal(err)
-    }
+	// Sorted so it can be searched later
+	sort.Strings(movieExtensions)
 
-    go func() {
-        for {
-            select {
-            case ev := <-watcher.Event:
-                if !ev.IsCreate() {
-                    continue
-                }
-                log.Println("event:", ev)
+	watcher, err := fsnotify.NewWatcher()
+	if err != nil {
+		log.Fatal(err)
+	}
 
-                ext := strings.ToLower(filepath.Ext(ev.Name))
-                if(ext == "") {
-                    continue
-                }
-                ext = ext[1:]
-                i := sort.SearchStrings(movieExtensions, ext)
-                if i < len(movieExtensions) && movieExtensions[i] == ext {
-                    HandleFile(ev.Name, outputPath, *deleteOnCompletion)
-                }
-            case err := <-watcher.Error:
-                log.Println("error:", err)
-            }
-        }
-    }()
+	go func() {
+		for {
+			select {
+			case ev := <-watcher.Event:
+				if !ev.IsCreate() {
+					continue
+				}
+				log.Println("event:", ev)
 
-    go ui.Run(5000, moviePath, inputPath, movieExtensions)
+				ext := strings.ToLower(filepath.Ext(ev.Name))
+				if ext == "" {
+					continue
+				}
+				ext = ext[1:]
+				i := sort.SearchStrings(movieExtensions, ext)
+				if i < len(movieExtensions) && movieExtensions[i] == ext {
+					HandleFile(ev.Name, outputPath, *deleteOnCompletion)
+				}
+			case err := <-watcher.Error:
+				log.Println("error:", err)
+			}
+		}
+	}()
 
-    // Iterate through and process any existing movie files
-    for _, file := range common.FindFileTypes(inputPath, movieExtensions) {
-        HandleFile(file, outputPath, *deleteOnCompletion)
-    }
+	go ui.Run(5000, moviePath, inputPath, movieExtensions, *staticPath)
 
-    err = watcher.Watch(inputPath)
-    if err != nil {
-        log.Fatal(err)
-    }
-    log.Println("Watching ", inputPath)
-    for {
-        time.Sleep(10000 * time.Millisecond)
-    }
-    watcher.Close()
+	// Iterate through and process any existing movie files
+	for _, file := range common.FindFileTypes(inputPath, movieExtensions) {
+		HandleFile(file, outputPath, *deleteOnCompletion)
+	}
+
+	err = watcher.Watch(inputPath)
+	if err != nil {
+		log.Fatal(err)
+	}
+	log.Println("Watching ", inputPath)
+	for {
+		time.Sleep(10000 * time.Millisecond)
+	}
+	watcher.Close()
 }
--- ui/web.go
+++ ui/web.go
@@ -1,177 +1,180 @@
 package ui
 
-import ("net/http"
-        "strconv"
-        "fmt"
-        "log"
-        "os"
-        "path/filepath"
-        "strings"
-        "sort"
-        "encoding/json"
-        "github.com/tangerilli/handbraked/common"
-        "code.google.com/p/go.net/websocket"
+import (
+	"code.google.com/p/go.net/websocket"
+	"encoding/json"
+	"fmt"
+	"github.com/tangerilli/handbraked/common"
+	"log"
+	"net/http"
+	"os"
+	"path/filepath"
+	"sort"
+	"strconv"
+	"strings"
 )
 
 type Handler struct {
+	staticPath string
 }
 
 func (p Handler) ServeHTTP(w http.ResponseWriter, r *http.Request) {
-    http.ServeFile(w, r, "static/index.html")
+	http.ServeFile(w, r, p.staticPath+"/index.html")
 }
 
 func staticHandler(w http.ResponseWriter, r *http.Request) {
-    path := r.URL.String()[1:]
-    http.ServeFile(w, r, path)
+	path := r.URL.String()[1:]
+	http.ServeFile(w, r, path)
 }
+
 type SourceFilesHandler struct {
-    sourcePath string
-    extensions []string
+	sourcePath string
+	extensions []string
 }
 
 type File struct {
-    Name string
-    Path string
+	Name string
+	Path string
 }
 
 type Directory struct {
-    Name string
-    Files []File
-    Children []Directory
+	Name     string
+	Files    []File
+	Children []*Directory
 }
 
 func getDirectory(path string, root *Directory) *Directory {
-    // Unix only for now
-    dirpath, _ := filepath.Split(path)
-    components := strings.Split(dirpath, "/")
-    if len(components) == 1 {
-        return root
-    }
+	// Unix only for now
+	dirpath, _ := filepath.Split(path)
+	components := strings.Split(dirpath, "/")
+	if len(components) == 1 {
+		return root
+	}
 
-    current := root
-    for _, component := range components[1:] {
-        if component == "" {
-            continue
-        }
-        var target *Directory = nil
-        for i:=0; i<len(current.Children); i++ {
-            cur := &current.Children[i]
-            if cur.Name == component {
-                target = cur
-                break
-            }
-        }
-        if target == nil {
-            target = &Directory{Name:component, Files:make([]File, 0), Children:make([]Directory, 0)}
-            current.Children = append(current.Children, *target)
-        }
-        current = target
-    }
-    return current
+	current := root
+	for _, component := range components[1:] {
+		if component == "" {
+			continue
+		}
+		var target *Directory = nil
+		for i := 0; i < len(current.Children); i++ {
+			cur := current.Children[i]
+			if cur.Name == component {
+				target = cur
+				break
+			}
+		}
+		if target == nil {
+			target = &Directory{Name: component, Files: make([]File, 0), Children: make([]*Directory, 0)}
+			current.Children = append(current.Children, target)
+		}
+		current = target
+	}
+	return current
 }
 
 func (p SourceFilesHandler) ServeHTTP(w http.ResponseWriter, r *http.Request) {
-    var root Directory
-    root.Name = "/"
-    filepath.Walk(p.sourcePath, func(path string, info os.FileInfo, err error) error {
-        localPath := strings.Replace(path, p.sourcePath, "", -1)
-        dir := getDirectory(localPath, &root)
+	var root Directory
+	root.Name = "/"
+	filepath.Walk(p.sourcePath, func(path string, info os.FileInfo, err error) error {
+		localPath := strings.Replace(path, p.sourcePath, "", -1)
+		dir := getDirectory(localPath, &root)
 
-        ext := strings.ToLower(filepath.Ext(path))
-        if ext == "" {
-            return nil
-        }
-        // Get rid of the leading '.'
-        ext = ext[1:]
-        i := sort.SearchStrings(p.extensions, ext)
-        if i >= len(p.extensions) || p.extensions[i] != ext {
-            return nil
-        }
-        f := File{filepath.Base(localPath), localPath}
-        dir.Files = append(dir.Files, f)
-        return nil
-    })
-    encoder := json.NewEncoder(w)
-    encoder.Encode(root)
+		ext := strings.ToLower(filepath.Ext(path))
+		if ext == "" {
+			return nil
+		}
+		// Get rid of the leading '.'
+		ext = ext[1:]
+		i := sort.SearchStrings(p.extensions, ext)
+		if i >= len(p.extensions) || p.extensions[i] != ext {
+			return nil
+		}
+		f := File{filepath.Base(localPath), localPath}
+		dir.Files = append(dir.Files, f)
+		return nil
+	})
+	encoder := json.NewEncoder(w)
+	encoder.Encode(root)
 }
 
 type queueRequest struct {
-    Path string
+	Path string
 }
 
 type QueueRequestHandler struct {
-    sourcePath string
-    watcherPath string
-    extensions []string
+	sourcePath  string
+	watcherPath string
+	extensions  []string
 }
 
 type QueueItem struct {
-    Name string
+	Name string
 }
 
 func (p QueueRequestHandler) ServeHTTP(w http.ResponseWriter, r *http.Request) {
-    if r.Method == "POST" {
-        decoder := json.NewDecoder(r.Body)
-        var request queueRequest
-        err := decoder.Decode(&request)
-        if err != nil {
-            fmt.Println(err)
-            panic(1)
-        }
-        path := filepath.Join(p.sourcePath, request.Path)
-        fmt.Println("Need to symlink " + path + " to " + p.watcherPath)
-        err = os.Symlink(path, filepath.Join(p.watcherPath, filepath.Base(path)))
-        if err != nil {
-            fmt.Println("Error!")
-        }
-    }
+	if r.Method == "POST" {
+		decoder := json.NewDecoder(r.Body)
+		var request queueRequest
+		err := decoder.Decode(&request)
+		if err != nil {
+			fmt.Println(err)
+			panic(1)
+		}
+		path := filepath.Join(p.sourcePath, request.Path)
+		fmt.Println("Need to symlink " + path + " to " + p.watcherPath)
+		err = os.Symlink(path, filepath.Join(p.watcherPath, filepath.Base(path)))
+		if err != nil {
+			fmt.Println("Error!")
+		}
+	}
 
-    if r.Method == "GET" {
-        var items []QueueItem
-        for _, file := range common.FindFileTypes(p.watcherPath, p.extensions) {
-            items = append(items, QueueItem{filepath.Base(file)})
-        }
-        encoder := json.NewEncoder(w)
-        encoder.Encode(items)
-    }
+	if r.Method == "GET" {
+		var items []QueueItem
+		for _, file := range common.FindFileTypes(p.watcherPath, p.extensions) {
+			items = append(items, QueueItem{filepath.Base(file)})
+		}
+		encoder := json.NewEncoder(w)
+		encoder.Encode(items)
+	}
 }
 
 type connection struct {
-    // The websocket connection.
-    ws *websocket.Conn
+	// The websocket connection.
+	ws *websocket.Conn
 
-    // Buffered channel of outbound messages.
-    send chan string
+	// Buffered channel of outbound messages.
+	send chan string
 }
 
 func (c *connection) reader() {
-    for {
-        var message string
-        err := websocket.Message.Receive(c.ws, &message)
-        if err != nil {
-            break
-        }
-        MessageHub.Broadcast <- message
-    }
-    c.ws.Close()
+	for {
+		var message string
+		err := websocket.Message.Receive(c.ws, &message)
+		if err != nil {
+			break
+		}
+		MessageHub.Broadcast <- message
+	}
+	c.ws.Close()
 }
 
 func (c *connection) writer() {
-    for message := range c.send {
-        err := websocket.Message.Send(c.ws, message)
-        if err != nil {
-            break
-        }
-    }
-    c.ws.Close()
+	for message := range c.send {
+		err := websocket.Message.Send(c.ws, message)
+		if err != nil {
+			break
+		}
+	}
+	c.ws.Close()
 }
 
 func statusHandler(ws *websocket.Conn) {
-    c := &connection{send: make(chan string, 256), ws: ws}
-    MessageHub.register <- c
-    defer func() { MessageHub.unregister <- c }()
-    go c.writer()
-    c.reader()
+	c := &connection{send: make(chan string, 256), ws: ws}
+	MessageHub.register <- c
+	defer func() { MessageHub.unregister <- c }()
+	go c.writer()
+	c.reader()
 }
 
 // For status:
@@ -183,22 +186,24 @@ func statusHandler(ws *websocket.Conn) {
 // It can refresh the collection every 30s or so, or whenever something is queued
 // There should be a collection view which renders the collection as a series of progress bars
 // A websocket should be setup from /api/queue which finds the appropriate collection element and updates the model progress variable
-// which causes a re-render of the progress bar (which implies that each queueitem should have its own subviews) 
+// which causes a re-render of the progress bar (which implies that each queueitem should have its own subviews)
 
-func Run(port int, filePath string, inputPath string, extensions []string) {
-    // start the websocket hub
-    go MessageHub.run()
+func Run(port int, filePath string, inputPath string, extensions []string, staticPath string) {
+	// start the websocket hub
+	go MessageHub.run()
 
-    handler := Handler{}
-    http.Handle("/", handler)
-    http.HandleFunc("/static/", staticHandler)
-    sourceHandler := SourceFilesHandler{filePath, extensions}
-    http.Handle("/api/files/source", sourceHandler)
-    queueHandler := QueueRequestHandler{filePath, inputPath, extensions}
-    http.Handle("/api/queue", queueHandler)
-    http.Handle("/api/queue/status", websocket.Handler(statusHandler))
+	http.Handle("/static/", http.StripPrefix("/static/", http.FileServer(http.Dir(staticPath))))
 
-    listenAddr := ":" + strconv.FormatInt(int64(port), 10)
-    fmt.Println("Starting webserver on ", listenAddr)
-    log.Fatal(http.ListenAndServe(listenAddr, nil))
+	handler := Handler{staticPath}
+	http.Handle("/", handler)
+
+	sourceHandler := SourceFilesHandler{filePath, extensions}
+	http.Handle("/api/files/source", sourceHandler)
+	queueHandler := QueueRequestHandler{filePath, inputPath, extensions}
+	http.Handle("/api/queue", queueHandler)
+	http.Handle("/api/queue/status", websocket.Handler(statusHandler))
+
+	listenAddr := ":" + strconv.FormatInt(int64(port), 10)
+	fmt.Println("Starting webserver on ", listenAddr)
+	log.Fatal(http.ListenAndServe(listenAddr, nil))
 }
